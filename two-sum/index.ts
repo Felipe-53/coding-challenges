cf8009cdb9589ed815c5e4a00121764ac06649d2
Felipe-53 <felipeandresb97@gmail.com>
2023-05-23 19:48:25 -0300

    optimal two sum solution

--- two-sum/index.ts
+++ two-sum/index.ts
@@ -1,4 +1,19 @@
-function twoSum(nums: number[], target: number): number[] {
+function optimalTwoSum(nums: number[], target: number): number[] | undefined {
+  const numbersMap = new Map<number, number>();
+
+  for (let i = 0; i < nums.length; i++) {
+    const number = nums[i];
+    const missing = target - number;
+    const index = numbersMap.get(missing);
+    if (index !== undefined) {
+      return [i, index];
+    } else {
+      numbersMap.set(number, i);
+    }
+  }
+}
+
+function bruteForceTwoSum(nums: number[], target: number): number[] {
   const indices: Set<number> = new Set();
 
   for (let i = 0; i < nums.length; i++) {
